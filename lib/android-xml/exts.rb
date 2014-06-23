20ad9adb3dd16704db5623fed83f71c780edd155
Colin T.A. Gray <colinta@gmail.com>
2014-06-23 10:19:30 -0600

    more extensions

--- lib/android-xml/exts.rb
+++ lib/android-xml/exts.rb
@@ -4,12 +4,28 @@ class Numeric
     "#{self}pt"
   end
 
+  def mm
+    "#{self}mm"
+  end
+
+  def in
+    "#{self}in"
+  end
+
   def px
     "#{self}px"
   end
 
   def dp
     "#{self}dp"
+  end
+
+  def dip
+    "#{self}dip"
+  end
+
+  def sp
+    "#{self}sp"
   end
 
 end
